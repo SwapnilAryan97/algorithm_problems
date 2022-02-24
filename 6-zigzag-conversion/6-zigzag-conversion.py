70c69dc04f1da7247d457af4437643f4fa939492
Swapnil Aryan Sinha <saiamthere@gmail.com>
2022-02-24 01:29:03 -0500

    Time: 92 ms (45.73%), Space: 14.1 MB (79.78%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.py
@@ -0,0 +1,15 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows==1: return s
+        res = ""
+        cycle = 2*numRows - 2
+        for i in range(0,numRows):
+            j=i
+            #for j in range(i,len(s),j+cycle):
+            while j<len(s):
+                res = res + s[j]
+                next_j = (j-i)+cycle-i
+                if i!=0 and i!=numRows-1 and next_j<len(s):
+                    res = res + s[next_j]
+                j = j+cycle
+        return res
