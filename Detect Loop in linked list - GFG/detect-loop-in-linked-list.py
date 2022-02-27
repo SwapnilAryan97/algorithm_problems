d6af1f6c6c932f6d2a92daa23d6b7498094a9177
Swapnil Aryan Sinha <saiamthere@gmail.com>
2022-02-27 13:24:05 -0500

    Added solution - LeetHub

--- Detect Loop in linked list - GFG/detect-loop-in-linked-list.py
+++ Detect Loop in linked list - GFG/detect-loop-in-linked-list.py
@@ -11,15 +11,15 @@ class Solution:
     #Function to check if the linked list has a loop.
     def detectLoop(self, head):
         #code here
-        slow = head
-        fast = head
+        # slow = head
+        # fast = head
         
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            if slow==fast:
-                return True
-        return False
+        # while fast and fast.next:
+        #     slow = slow.next
+        #     fast = fast.next.next
+        #     if slow==fast:
+        #         return True
+        # return False
         
         temp = set()
         node = head
