3a0212498b4307b70b5e072805ff32cac29d250e
Swapnil Aryan Sinha <saiamthere@gmail.com>
2022-03-24 12:56:47 -0400

    Time: 178 ms (18.01%), Space: 14.6 MB (5.42%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,27 +1,13 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        table = [[]]*(target+1)
-        table[0] = [[]]
-        i=0
-        candidates.sort()
-        while i<=target:
-            for num in candidates:
-                if i+num<=target:
-                    temp = deepcopy(table[i])
-                    for j in range(len(temp)):
-                        temp[j].append(num)
-                        temp[j].sort()
-                    if len(table[i+num])==0:
-                        table[i+num] = temp
-                    else:
-                        for val in temp:
-                            if val in table[i+num]:
-                                continue
-                            else:
-                                table[i+num].append(val)
-                        # table[i+num].sort()
-                else:
-                    break
-            i+=1
-
-        return table[target]
+        dp = [[] for _ in range(target+1)]
+        # print(dp)
+        for c in candidates:
+            for i in range(c, target+1):
+                if i==c:
+                    dp[i].append([c])
+                for combo in dp[i-c]:
+                    # print('combo=',combo)
+                    dp[i].append(combo + [c])
+            print(dp)
+        return dp[-1]
