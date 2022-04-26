3262c353f293e30f8d71cd26248168343182d00b
Swapnil Aryan Sinha <saiamthere@gmail.com>
2022-04-26 19:12:29 -0400

    Time: 318 ms (76.23%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,8 +1,5 @@
 # Write your MySQL query statement below
-select email from (
-
-select email, count(email) as num
-from Person
-group by email
-) as statistic
-where num>1;
+SELECT email as Email
+FROM Person 
+GROUP BY email
+HAVING COUNT(*) > 1
