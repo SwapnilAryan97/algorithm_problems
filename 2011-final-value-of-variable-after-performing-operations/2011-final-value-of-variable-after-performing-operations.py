477395d85f4f23bdba939fb5487d3b347f990e31
Swapnil Aryan Sinha <saiamthere@gmail.com>
2022-04-26 19:41:09 -0400

    Time: 52 ms (89.52%), Space: 13.9 MB (59.38%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -1,15 +1,20 @@
 class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
-        res = 0
-        for str in operations:
-            if str[0]=='X':
-                if str[1]=='-':
-                    res -=1
-                if str[1]=='+':
-                    res +=1
+        op_dict = {"--X" : -1, "X--" : -1,
+                 "++X" : 1, "X++" : 1}
+        return sum(op_dict[op] for op in operations)
+        
+        
+#         res = 0
+#         for str in operations:
+#             if str[0]=='X':
+#                 if str[1]=='-':
+#                     res -=1
+#                 if str[1]=='+':
+#                     res +=1
             
-            elif str[0]=='-':
-                res -=1
-            else:
-                res +=1
-        return res
+#             elif str[0]=='-':
+#                 res -=1
+#             else:
+#                 res +=1
+#         return res
