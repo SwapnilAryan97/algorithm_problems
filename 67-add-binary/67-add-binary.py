ccfc4f19483ff4c5105f9361de18406a8a325f99
Swapnil Aryan Sinha <saiamthere@gmail.com>
2022-06-10 13:02:10 -0400

    Time: 69 ms (11.18%), Space: 13.8 MB (72.21%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,25 +1,18 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-#         a = list(a)
-#         b = list(b)
-#         carry=0
-#         res=""
+        a = list(a)
+        b = list(b)
+        carry=0
+        res=""
         
-#         while a or b or carry:
-#             if a:
-#                 carry+= int(a.pop())
-#             if b:
-#                 carry+= int(b.pop())
+        while a or b or carry:
+            if a:
+                carry+= int(a.pop())
+            if b:
+                carry+= int(b.pop())
             
-#             res += str(carry%2)
-#             carry //= 2
-#         return res[::-1]
+            res += str(carry%2)
+            carry //= 2
+        return res[::-1]
 
-        "Leetcode best solution wrt time"
-        a1 = int(a)
-        b1 = int(b)
-        total = 0
-        
-        sum = int(a, 2) + int(b, 2)
-        return bin(sum)[2:]
             
