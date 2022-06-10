86fa5b5e7909b7d19b71fc6ee78f60d87a32ca21
Swapnil Aryan Sinha <saiamthere@gmail.com>
2022-06-10 13:17:05 -0400

    Time: 69 ms (29.38%), Space: 13.9 MB (82.27%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.py
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.py
@@ -1,12 +1,21 @@
 class Solution:
     def mostWordsFound(self, sentences: List[str]) -> int:
-        # print(list(sentences[0]))
-        # print(sentences[0].strip(" ")[])
-        x=0
+        # x=0
+        # res = 0
+        # for i in range(len(sentences)): 
+        #     x+=sentences[i].strip().count(" ")
+        #     res = max(x+1,res)
+        #     x=0
+        # return res
+    
+        "Another solution"
         res = 0
-        for i in range(len(sentences)): 
-            x+=sentences[i].strip().count(" ")
-            res = max(x+1,res)
-            x=0
+        for x in sentences:
+            if len(x.split(' '))>res:
+                res = len(x.split(' '))
         return res
+
+        
+    
+    
         
