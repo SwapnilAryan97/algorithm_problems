1240f6980bf487d001dbd3d6e706b5ba126b6ae5
Swapnil Aryan Sinha <saiamthere@gmail.com>
2022-06-15 14:27:32 -0400

    Time: 630 ms (43.40%), Space: 26 MB (71.57%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,8 +1,3 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        mem = {}
-        for num in nums:
-            if num not in mem:
-                mem[num] = True
-            else:
-                return True
+        return len(nums)!=len(set(nums))
