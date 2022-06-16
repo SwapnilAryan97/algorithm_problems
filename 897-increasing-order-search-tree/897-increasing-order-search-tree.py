f7007a2fdbfb00f0d91b732d0b83f8538bf816ae
Swapnil Aryan Sinha <saiamthere@gmail.com>
2022-06-16 16:40:25 -0400

    Time: 53 ms (10.46%), Space: 14 MB (39.66%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.py
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.py
@@ -5,12 +5,29 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def increasingBST(self, root: TreeNode, tail = None) -> TreeNode:
-        if not root:
-            return tail
+    def inorder(self, root, arr):
+        if root:
+            self.inorder(root.left, arr)
+            arr.append(root.val)
+            self.inorder(root.right, arr)
+        return arr
+    
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        arr = self.inorder(root, [])
+        temp = TreeNode(arr[0])
+        node = temp
+        for num in arr[1:]:
+            node.right = TreeNode(num)
+            node = node.right
         
-        res = self.increasingBST(root.left, root)
-        root.left = None
-        root.right = self.increasingBST(root.right, tail)
+        return temp
         
-        return res
+        
+#         if not root:
+#             return tail
+        
+#         res = self.increasingBST(root.left, root)
+#         root.left = None
+#         root.right = self.increasingBST(root.right, tail)
+        
+#         return res
